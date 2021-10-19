c6074fd4c159aa9ce85a72f867b8f17dd973e8da
tanglz <lizhen.tang@gmail.com>
2021-10-19 10:16:43 -0400

    update

--- background.js
+++ background.js
@@ -1,43 +1,47 @@
-let color = '#c1913e';
-
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({color});
-});
-let current_url = "";
-let verify_result = 1;
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    current_url = tab.url;
-    let is_system_page = current_url.startsWith("chrome-extension") || current_url.startsWith("https://meet.google.com") || current_url.startsWith("chrome://newtab");
-    if (is_system_page) {
-        return;
-    }
-    chrome.storage.sync.set({current_url});
-    console.log(changeInfo)
-    if (changeInfo.status != 'complete') {
-        return;
-    }
-    const parameter = {url: current_url};
-    fetch('http://127.0.0.1:5000/predict/ml', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(parameter),
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.result) {
-                verify_result = -1
-                chrome.storage.sync.set({verify_result});
-            } else {
-                verify_result = 1
-                chrome.storage.sync.set({verify_result});
-            }
-            chrome.tabs.sendMessage(tabId, {
-                message: 'result',
-                url: tab.url,
-                result: verify_result
+const a =1;
+function fun_action(tabId) {
+    let current_url = "";
+    chrome.storage.sync.set({'data': {}});
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        console.log(tabs);
+        if (!tabs || tabs.length < 1) {
+            return;
+        }
+        current_url = tabs[0].url;
+        console.log(current_url)
+        if (!current_url) {
+            return;
+        }
+        chrome.storage.sync.set({current_url});
+        const parameter = {url: current_url};
+        fetch('http://35.224.126.121:5000/predict/ai', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(parameter),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                chrome.storage.sync.set({'data': data});
+                chrome.tabs.sendMessage(tabId, {
+                    message: 'result',
+                    url: current_url,
+                    result: data
+                });
             });
-        });
+    });
+}
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
+    console.log(changeInfo);
+    if (changeInfo.status == 'complete') {
+        fun_action(tabId);
+    }
+});
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    console.log(activeInfo)
+    console.log(activeInfo.tabId);
+    fun_action(activeInfo.tabId);
 });
 
--- popup.js
+++ popup.js
@@ -1,59 +1,64 @@
-// https://developer.chrome.com/docs/extensions/mv3/hosting/
-// Initialize butotn with users's prefered color
-// let changeColor = document.getElementById("changeColor");
-
-// chrome.storage.sync.get("color", ({ color }) => {
-//   changeColor.style.backgroundColor = color;
-// });
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-// changeColor.addEventListener("click", async () => {
-//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-//
-//   chrome.scripting.executeScript({
-//     target: { tabId: tab.id },
-//     function: setPageBackgroundColor,
-//   });
-// });
-
-// The body of this function will be execuetd as a content script inside the
-// current page
-// function setPageBackgroundColor() {
-//   chrome.storage.sync.get("color", ({ color }) => {
-//     document.body.style.backgroundColor = color;
-//   });
-// }
 let url_container = document.getElementById("detect_url");
 let link = "";
 chrome.storage.sync.get("current_url", ({ current_url }) => {
   link = current_url
   url_container.innerHTML = current_url;
 });
-let result_container = document.getElementById("verify_result");
 let action_container = document.getElementById("report_action");
 let error_type=1
-chrome.storage.sync.get("verify_result", ({ verify_result }) => {
-  console.log(verify_result)
-  verify_html="";
-  if(verify_result==1){
-    verify_html="<div class='legitimate'>We did not find any fraudulent elements on this page.</div>";
-    action_text="Report this site as suspicious >>";
-    // card_container.classList.add('bg-success');
-    // card_container.classList.remove('bg-danger');
-    url_container.style.color='green';
-    url_container.classList.add('yes');
-    url_container.classList.remove('no');
-    action_container.href='http://127.0.0.1:5000/verify/add?error_type=1&url='+link
+
+chrome.storage.sync.get("data", ({ data }) => {
+  if(data && data.success){
+    console.log(data)
+    verify_html="";
+    if(!data.phishing){
+      // verify_html="<div class='legitimate'>We did not find any fraudulent elements on this page.</div>";
+      action_text="Report this site as suspicious >>";
+      // card_container.classList.add('bg-success');
+      // card_container.classList.remove('bg-danger');
+      url_container.style.color='green';
+      url_container.classList.add('yes');
+      url_container.classList.remove('no');
+      url_container.classList.remove('warn');
+      action_container.href='http://35.224.126.121:5000/verify/add?error_type=1&url='+link
+    }
+    if(data.phishing){
+      // verify_html="<div class='phishing'>Attackers may trick you into doing something dangerous like installing software or revealing your personal information (for example, passwords, phone numbers, or credit cards).</div>";
+      action_text="False Alarm >>";
+      error_type=2
+      if(data.risk >0.99){
+        url_container.style.color='red';
+        url_container.classList.add('no');
+      }else{
+        url_container.style.color='orangered';
+        url_container.classList.add('warn');
+      }
+      url_container.classList.remove('yes');
+      action_container.href='http://35.224.126.121:5000/verify/add?error_type=2&url='+link
+    }
+    document.getElementById("netloc-country").innerText = data.net_info.country;
+    document.getElementById("netloc-domain").innerText = data.net_info.domain;
+    document.getElementById("netloc-years").innerText = data.net_info.years;
+    document.getElementById("netloc-org").innerText = data.net_info.org;
+    const logos = data.logos
+    if(logos&&logos.length>=1){
+      document.getElementById("netloc-logo").innerText = logos[0];
+    }
+    action_container.innerText = action_text;
+    const risk = data.risk
+    let risk_text = "LOW"
+    let text_color = "green"
+    if(risk>=0.99){
+        risk_text = "HIGH"
+        text_color = "red"
+    }
+    if(risk<0.99 && risk>=0.85){
+        risk_text = "MEDIUM"
+        text_color = "orangered"
+    }
+    document.getElementById("phishing-level").innerText = risk_text
+    document.getElementById("phishing-level").style.color = text_color
+    document.getElementById("risk-more").href = 'http://35.224.126.121:5000/url/net_loc?url='+link
   }
-  if(verify_result==-1){
-    verify_html="<div class='phishing'>Attackers may trick you into doing something dangerous like installing software or revealing your personal information (for example, passwords, phone numbers, or credit cards).</div>";
-    action_text="False Alarm >>";
-    error_type=2
-    url_container.style.color='red';
-    url_container.classList.add('no');
-    url_container.classList.remove('yes');
-    action_container.href='http://127.0.0.1:5000/verify/add?error_type=2&url='+link
-  }
-  result_container.innerHTML = verify_html;
-  action_container.innerText = action_text;
+
 });
